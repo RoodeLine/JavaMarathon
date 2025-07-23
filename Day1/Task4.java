3a8e0563c50a61ea8f9929685904cb277b63fc52
charlsih <charlsih@student.21-school.ru>
2025-07-23 14:54:53 +0300

    Day1

---
+++ Day1/Task4.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int age = 46;
+        while (age <= 100) {
+            System.out.println("Мой возраст " + age);
+            age += 5;
+    }
+    }
+}
---
+++ Day1/Task5.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        int age = 46;
+        for (int i = age; i <= 100; i+=5) {
+            System.out.println("Мой возраст " + i);
+        }
+    }
+}
